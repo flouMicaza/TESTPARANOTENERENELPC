11dd3e5a45f75e576cba2f9f997b690c44a9237b
flouMicaza <flo.mirandai@gmail.com>
2017-12-28 15:41:50 -0300

    holiwi

---
+++ _test_/environment.test.js
@@ -0,0 +1,10 @@
+/*import React from 'react';
+import Environment from '../Environment';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Environment>PRUEBAAAA</Environment>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});*/
